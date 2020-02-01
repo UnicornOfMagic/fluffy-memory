5672d01e078e7b43a1a11c4828739f0783712b5f
Brian Gerhards <briangerhards@gmail.com>
2020-02-01 15:30:46 -0600

    adds game pause menu

---
+++ Assets/Scripts/GamePauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamePauseMenu : MonoBehaviour
+{
+    [SerializeField]
+    GameObject pauseMenu;
+
+    [SerializeField]
+    bool isShowing = false;
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void ToggleMenuActive()
+    {
+        isShowing = !isShowing;
+        pauseMenu.SetActive(isShowing);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            ToggleMenuActive();
+        }
+    }
+
+
+}
