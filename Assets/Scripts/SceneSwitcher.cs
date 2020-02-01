d9388e0dc204e7df3d172badc66c0eb1f8445e09
Jared Green <jaredgreen23@outlook.com>
2020-01-31 23:34:11 -0600

    adds SceneSwitcher and navigation on menu start button click

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GotoGameScene()
+    {
+        SceneManager.LoadScene("GameScene001");
+    }
+
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+}
