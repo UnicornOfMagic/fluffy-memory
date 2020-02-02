cca6fe9165a6f3051854c3bc8d7064efc7108ef9
Brian Gerhards <briangerhards@gmail.com>
2020-02-01 18:27:51 -0600

    update owned by client flag on balls

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -35,7 +35,7 @@ namespace Normal.Realtime.Examples
                 }
             }
 
-            InstantiateNewObjectInWorld(playerTypeToLoad, realtime);
+            InstantiateNewObjectInWorld(playerTypeToLoad, realtime, true);
         }
 
         private void CreateBall(Realtime realtime)
@@ -43,13 +43,13 @@ namespace Normal.Realtime.Examples
             InstantiateNewObjectInWorld("Sphere", realtime);
         }
 
-        private void InstantiateNewObjectInWorld(string prefab, Realtime realtime)
+        private void InstantiateNewObjectInWorld(string prefab, Realtime realtime, bool ownedByClient = false)
         {
             Vector3 startingPosition = new Vector3(Random.Range(-4, 4),1, Random.Range(-4, 4 ));
             Realtime.Instantiate(prefab,                     // Prefab name
                                 position: startingPosition,          // Start 1 meter in the air
                                 rotation: Quaternion.identity, // No rotation
-                           ownedByClient: true,                // Make sure the RealtimeView on this prefab is owned by this client
+                           ownedByClient: ownedByClient,                // Make sure the RealtimeView on this prefab is owned by this client
                 preventOwnershipTakeover: true,                // Prevent other clients from calling RequestOwnership() on the root RealtimeView.
                              useInstance: realtime);           // Use the instance of Realtime that fired the didConnectToRoom event.
         }
